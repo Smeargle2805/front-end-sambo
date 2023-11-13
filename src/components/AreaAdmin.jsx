91460c1f601bb61933e5d1915a77bbab9522751e
CARDONA LUIS <kikecar97@gmail.com>
2023-11-13 14:39:44 -0600

    Maestros
    creación pagina maestros
    logo de sesion

--- src/components/AreaAdmin.jsx
+++ src/components/AreaAdmin.jsx
@@ -1,9 +1,11 @@
 import React, { useState } from 'react'
 import './css/areaAdmin.css'
 import samboLogo from'../assets/samboLogo.jpeg'
+import LoginOutLogo from '../assets/user.png'
 import { Usuarios } from "./Usuarios"
 import { Atletas } from './Atletas'
 import{ SubirPost } from './SubirPost'
+import { Maestros } from './Maestros'
 
 export const AreaAdmin = () => {
 
@@ -17,10 +19,17 @@ export const AreaAdmin = () => {
   const hacerPost = () => {
     setComponenteActual(<SubirPost/>)
   }
+  const mostrarMaestros = () => {
+    setComponenteActual(<Maestros/>)
+  }
   return (
     <>
       <section id='area-total' className='w-100 h-100'>
         <div id='barra-lateral'>
+          <div id='logo-User'>
+            <img src={LoginOutLogo}/>
+            <p>Cerrar Sesion</p>
+          </div>
           <div id='info-user' className='d-flex align-items-center justify-content-center flex-column'>
               <img src={samboLogo} alt="" />
               <p>Usuario1</p>
@@ -33,7 +42,7 @@ export const AreaAdmin = () => {
               <button className='button-opcion' type='button' onClick={mostrarAtletas}>Atletas</button>
             </div>
             <div className='w-100 h-100 d-flex justify-content-center'>
-              <button className='button-opcion' type='button'>Maestros</button>
+              <button className='button-opcion' type='button' onClick={mostrarMaestros}>Maestros</button>
             </div>
             <div className='w-100 h-100 d-flex justify-content-center'>
               <button className='button-opcion' type='button'>Escuelas</button>
