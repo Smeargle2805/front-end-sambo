47b7169013b04b081dabaf2d0ac5f65ec17591fc
vspeed21 <vt96902@gmail.com>
2023-11-16 15:37:05 -0600

    main page terminado

--- src/components/MainPage.jsx
+++ src/components/MainPage.jsx
@@ -1,76 +1,77 @@
-import React from 'react'
-import './css/mainpage.css'
+import './css/mainpage.css';
+import { Link } from 'react-router-dom'
+
 import logo from '../assets/LOGO.png'
-import img1 from '../assets/atletas/presi1.jpeg'
-import img2 from '../assets/atletas/brad.jpeg'
-import img3 from '../assets/atletas/molina.jpeg'
+import bannerImg from '../assets/banner.jpg'
+import acercaDeImg from '../assets/acercade.jpg'
+
 export const MainPage = () => {
+  const year = new Date().getFullYear();
+
   return (
     <>
-    <nav>
-        <div>
-            <img src={logo} alt="" />
+      <div className='p-0 mt-0 row nav-azul justify-content-center align-items-center'>
+        <div className='col-md-4'></div>
+        <div className='col-md-4 d-flex justify-content-center'>
+          <img src={logo} alt='logo' title='logo' className='logo' />
         </div>
-    </nav>
-    <section>
-        <div id='info'>
-            <div>
-            <div class="card" style="width: 18rem;">
-  <img src="..." class="card-img-top" alt="..."/>
-  <div class="card-body">
-    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-  </div>
-</div>
-            </div>
-            <div></div>
+        <div className='col-md-4'>
+          <Link
+            className='text-white btn-login d-block'
+            to='/login'
+          >
+            Iniciar sesión
+          </Link>
+        </div>
+      </div>
+
+      <img src={bannerImg} alt='logo' className='banner' />
+
+      <main className='mt-3 row contenedor'>
+        <div className='col-md-6'>
+          <h2 className='font-bold text-center'>Noticias</h2>
+          <div className='contenido'>
+            <ul>
+              <li>Noticia 1</li>
+              <li>Noticia 2</li>
+              <li>Noticia 3</li>
+            </ul>
+          </div>
+        </div>
+        <div className='col-md-6'>
+          <h2 className='font-bold text-center'>Ultimas participaciones</h2>
+          <div className='contenido'>
+            <ul>
+              <li>Ulimas participaciones 1</li>
+              <li>Ulimas participaciones 2</li>
+              <li>Ulimas participaciones 3</li>
+            </ul>
+          </div>
         </div>
 
-        <div id='banner'>
-            <div>
-            <div id="miCarrusel" class="carousel slide" data-bs-ride="carousel">
-                <div class="carousel-inner">
-                    <div class="carousel-item active">
-                        <div className='grid-banner'>
-                            <div className='div-img-banner'>
-                                <img src={img1} alt="Imagen 1"/>
-                            </div>
-                            <div className='info-atleta'>
-                            <h4 className='tex'>El presi</h4>
-                                <p>Segundo lugar panamericano de sambo en la categoria de +98 kg</p>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="carousel-item">
-                        <div className='grid-banner'>
-                            <div className='div-img-banner'>
-                                <img src={img2} alt="Imagen 2"/>
-                            </div>
-                            <div className='info-atleta'>
-                                <h4 className='tex'>Brad Flores</h4>
-                                <p>Segundo lugar panamericano de sambo en la categoria de +98 kg</p>
-                                
-                            </div>
-                        </div>
-                    </div>
-                    <div class="carousel-item">
-                    <div className='grid-banner'>
-                            <div className='div-img-banner'>
-                                <img src={img3}  alt="Imagen 3"/>
-                            </div>
-                            <div className='info-atleta'>
-                                <h4 className='tex'>Francisco Molina</h4>
-                                <p>segundo lugar panamericano de sambo en la categoria de +98 kg</p>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-            </div>
-            <div>
+      </main>
 
-            </div>
+      <hr />
+
+      <div className='my-5 row justify-content-center align-items-center contenedor'>
+        <div className='col-md-5'>
+          <h2 className='text-center heading'>Federación Hondureña de Sambo</h2>
+          <p>
+            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Hic illum optio obcaecati in eligendi nisi, quidem, neque voluptatibus omnis laborum dolorem sit, nobis veritatis.
+          </p>
         </div>
-    </section>
+        <div className='col-md-7 d-flex justify-content-center'>
+          <img src={acercaDeImg} alt='imagen acerca de' title='imagen acerca de' />
+        </div>
+      </div>
+
+      <footer className='p-3 bg-white border shadow-sm'>
+        <p className='text-center copyright'>
+          Copyright &copy; {''}
+          <span className='year'>{year}</span> {''}
+          Todos los derechos resevados
+        </p>
+      </footer>
 
     </>
   )
