1e85e8803c0cab543872609ea6f14fa0ff574374
vspeed21 <vt96902@gmail.com>
2023-11-16 15:37:28 -0600

    mejorar las rutas del admin

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
-
+// Views
 import { AreaAdmin } from './components/AreaAdmin';
 import { Login } from './components/login';
 import { Atletas } from './components/Atletas';
@@ -9,6 +9,7 @@ import { Maestros } from './components/Maestros';
 import { Escuelas } from './components/Escuelas';
 import { Torneos } from './components/Torneos';
 import { SubirPost } from './components/SubirPost';
+import { MainPage } from './components/MainPage';
 
 export const App = () => {
   return (
@@ -16,17 +17,16 @@ export const App = () => {
       <BrowserRouter>
         <Routes>
           <Route path='/login' element={<Login />} />
+          <Route path='/' element={<MainPage />} />
+          <Route path='/admin/*' element={<AreaAdmin />}>
+            <Route path='usuarios' element={<Usuarios />} />
+            <Route path='atletas' element={<Atletas />} />
+            <Route path='maestros' element={<Maestros />} />
+            <Route path='escuelas' element={<Escuelas />} />
+            <Route path='torneos' element={<Torneos />} />
+            <Route path='editor' element={<SubirPost />} />
+          </Route>
         </Routes>
-        <AreaAdmin>
-          <Routes>
-            <Route path='/usuarios' element={<Usuarios />} />
-            <Route path='/atletas' element={<Atletas />} />
-            <Route path='/maestros' element={<Maestros />} />
-            <Route path='/escuelas' element={<Escuelas />} />
-            <Route path='/torneos' element={<Torneos />} />
-            <Route path='/editor' element={<SubirPost />} />
-          </Routes>
-        </AreaAdmin>
       </BrowserRouter>
     </>
   )
